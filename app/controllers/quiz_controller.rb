e3b7da2ee9db59be3428c033cd9535ce5f24654e
icecoldmax <david.nettle@gmail.com>
2012-07-28 00:59:23 +0900

    Removes the pitiful attempt to rubify the quiz. JS ftw

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -1,13 +1,21 @@
 class QuizController < ApplicationController
 	def index
-		if params[:commit]
-			@quiz_options = {}
-			@videos = params[:videos]
+		# if params[:commit]
+		# 	@quiz_options = {}
+		# 	@videos = params[:videos]
+		# 	@question_types = []
 
-			@quiz_options[:add] = { start: params[:add][:start], end: params[:add][:end] } if params[:add][:on] == '1'
-			@quiz_options[:sub] = { start: params[:sub][:start], end: params[:sub][:end] } if params[:sub][:on] == '1'
+		# 	if params[:add][:on] == '1'
+		# 		@quiz_options[:add] = { start: params[:add][:start], end: params[:add][:end] }
+		# 		@question_types.push("add")
+		# 	end
+
+		# 	if params[:sub][:on] == '1'
+		# 		@quiz_options[:sub] = { start: params[:sub][:start], end: params[:sub][:end] }
+		# 		@question_types.push("sub")
+		# 	end
 			
-		end
+		# end
 	end
 		
 end
